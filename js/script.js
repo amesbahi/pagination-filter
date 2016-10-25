07da2d4d483367ee4d163cc74108206fe78413ed
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2016-10-24 22:13:02 -0400

    Paginated search results

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ $searchButton[0].addEventListener("click", function() {
   // Search functionality
   var $students = $(".student-item"); // arry containing all of the students
   var searchString = $searchInput.val().toLowerCase(); // storing the search value in a variable
+  var studentsArray = []; // add students to this array when user searches for students
   $.each($students, function(index, student) {  // Looping over the students
     console.log(index, student);
     var $student = $(student);
@@ -35,22 +36,36 @@ $searchButton[0].addEventListener("click", function() {
     console.log(findStudent, findEmail);
     if (findStudent.indexOf(searchString) >= 0 || findEmail.indexOf(searchString) >= 0) {
       $(student).show();
+      studentsArray.push($(student));
     } else {
       $(student).hide();
+      $("body").html("<h1>No results found.</h1>");
     }
   });
 
-  searchPaginationResults();
+  searchPaginationResults(studentsArray);
 });
 
-var searchPaginationResults = function() {
+
+var $allStudentsGlobal = $('.student-item');
+searchPaginationResults($allStudentsGlobal);
+
+// loop through all the students
+// reset the classes for the students "student-item cf"
+function resetClassName() {
+  for (var i = 0; i < $allStudentsGlobal.length; i++) {
+  var aStudent = $allStudentsGlobal[i];
+  aStudent.className = "student-item cf";
+  }
+}
+
+function searchPaginationResults($paginatedStudents) {
     // Pagination Functionality
   var studentsPerPage = 10 // Number of students per page to render
-  var $allStudents = $('.student-item');
-  var numberOfPages = Math.ceil($allStudents.length / studentsPerPage);
+  var numberOfPages = Math.ceil($paginatedStudents.length / studentsPerPage);
   console.log(numberOfPages);
 
-  // HTML for pagination
+  // HTML for pagination at the footer
   var paginationHTML = '<ul>';
   for (var i = 0; i < numberOfPages; i++) {
     paginationHTML += '<li onclick="onPageClick(' + (i + 1) +  ')"><a class="pagination">' + (i + 1) + '</a></li>\n';
@@ -58,21 +73,23 @@ var searchPaginationResults = function() {
   paginationHTML += '</ul>';
   $("#pagination").html(paginationHTML);
 
+// Calling the resetClassName function
+resetClassName();
+
 
   // create for loop adding order of pagination classes to each student
   var currentPageNum = 0;
-  for (var i = 0; i < $allStudents.length; i++) {
-    var student = $allStudents[i];
+  for (var i = 0; i < $paginatedStudents.length; i++) {
+    var student = $paginatedStudents[i];
     console.log(student);
     // every 10 iterations of loop bump up currentPageNum by 1
     if (i % 10 === 0) {
       currentPageNum += 1;
     }
-    student.className += " page-" + currentPageNum;
+    $(student).addClass("page-" + currentPageNum);
   }
 }
 
-searchPaginationResults();
 
 
 
@@ -107,3 +124,11 @@ function addThreeNums(x, y, z) {
 // capture the search results
 // pass that into the searchPaginationResults function
 // display paginated results if there are more than 10 students
+
+
+
+
+
+
+
+// add new page numbers to the students
